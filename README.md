e0abeb2eb86033a727ef3a5bb3306a6853dd333f
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2017-12-01 16:08:14 +1300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,163 @@
 # gulp-single-file-components
+
+A Gulp plugin that let's you build single file components (as made famous by Vue) and use them in other environments by splitting the vue-files into separate files.
+
+## Prerequisites
+
+* Works in Node 4.7.2+ with Gulp.js.
+
+## Introduction
+
+I created this to be able to improve how I worked with modules with Wordpress and the system I was using at the time made for a very long and complicated build process. This plugin however let me simplify the build process significantly and components were then assembled and included by a custom library.
+
+Since I made it to meet a personal need it's not very elegant, it currently monkey-patches the package [vueify](https://www.npmjs.com/package/vueify) and hooks into its compiler. If it turns out that other people than me starts using the package I'll probably make a proper fork in the future.
+
+## Usage
+
+As simple as it gets:
+
+```js
+const gulp = require('gulp');
+const components = require('gulp-single-file-components');
+
+gulp.task('components', function() {
+    return gulp.src('components/**/*.vue')
+      .pipe(components())
+      .pipe(gulp.dest('dist/'));
+});    
+```
+
+The code above would output the follow vue-file into 3 different files with same name but corresponding extension:
+```php
+<template>
+	<h1>Template</h1>
+</template>
+
+<script>
+    console.log('script');
+</script>
+
+<style lang="scss">
+	h1 {
+		font-weight: bold;
+	}
+</style>
+```
+
+Since we're hooking into vueify's compiler we get [all the features from it](https://www.npmjs.com/package/vueify) as well, including support for different languages, like *SCSS* in the above example's style tag.
+
+## Features
+
+You can also extend the features of the plugin by passing in some options. Every option that's not handled by the plugin itself gets passed on to the vueify compiler so you can configure it too. Here's an example of adding a PHP compiler, a custom tag, and filtering the output from the default JavaScript compiler:
+
+```js
+const gulp = require('gulp');
+const deindent = require('de-indent');
+const indent = require('indent-string');
+const components = require('gulp-single-file-components');
+
+gulp.task('components', function() {
+    return gulp.src('components/**/*.vue')
+      .pipe(components({
+        // We add a php compiler
+        // (this get's passed on to vueify)
+        customCompilers: {
+          php: function (content, cb, compiler, filePath) {
+            content = content.trim();
+
+            if (content.startsWith('<?php')) {
+              content = content.slice(5);
+            }
+            if (content.startsWith('<?')) {
+              content = content.slice(2);
+            }
+            if (content.endsWith('?>')) {
+              content = content.slice(0, -2);
+            }
+
+            let result = '<?php \n\n'+deindent(content).trim()+'\n';
+            cb(null, result);
+          },
+        },
+        // We add a custom tag named "config"
+        // (this callback sets the file extension for the custom tag)
+        tags: {
+          config: function (lang, filePath, node) {
+            if (! lang) {
+              return 'ini';
+            }
+            else {
+              return (lang === 'php') ? 'config.'+lang : lang;
+            }
+          },
+        },
+        // We wrap the default output of the vueify js compiler
+        // with a self executing function
+        outputModifiers: {
+          script: function (content, lang) {
+            return (! lang) ? '(function() { \n'+indent(content, 2)+'\n})();' : content;
+          },
+        }
+      }))
+      .pipe(gulp.dest('dist/');
+});    
+```
+
+Here's an example of what the above modifications would enable. The following vue-file (example.vue):
+
+```php
+<template lang="blade.php">
+	<h1>{{ $title }}</h1>
+</template>
+
+<script>
+    console.log('script');
+</script>
+
+<style lang="scss">
+    h1 {
+        font-weight: bold;
+    }
+</style>
+
+<config lang="php">
+	return [
+		'title' => __('Template', 'theme'),
+    ];
+</config>
+```
+
+Would output the following 4 files:
+
+**example.blade.php**
+```php
+<h1>{{ $title }}</h1>
+```
+**example.js**
+```js
+(function() {
+    "use strict";
+
+    console.log('script');
+})();
+```
+**example.css**
+```css
+h1 {
+    font-weight: bold;
+}
+```
+**example.config.php**
+```php
+<?php
+
+return [
+    'title' => __('Template', 'theme'),
+];
+```
+
+By default the `template` tag's resulting extension is simply taken from the lang-attribute. Which is why we can output the template as blade.php without creating a custom tag handler (however it's not compiled blade, that would require a custom compiler). This can be changed by overriding the extension callback in the `tags` option. By default `style` only returns css and `script` js.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,241 @@
+"use strict";
+
+const PLUGIN_NAME = 'gulp-single-file-components';
+const PLUGIN_DEBUG = false;
+
+// Globals
+var through = require('through2');
+var PluginError = require('gulp-util').PluginError;
+var path = require('path');
+var vinylSource = require('vinyl-source-stream');
+var strStream = require('string-to-stream');
+var defaults = require('defaults');
+
+// Vueify Dependencies
+var parse5 = require('parse5');
+var hash = require('hash-sum');
+var deindent = require('de-indent');
+
+// Monkey-patch Vueify
+let rewire = require('rewire');
+let vueifyDir = path.dirname(require.resolve('vueify'));
+let compilerPath = path.join(vueifyDir, 'lib/compiler.js');
+var compiler = rewire(compilerPath);
+
+// Main
+module.exports = function(args) {
+  debug(PLUGIN_NAME);
+
+  // These are used to track what extension the output file should have as well as
+  // what language has been identified for it
+  var extensions, languages = {
+    template: null,
+    script: null,
+    style: null,
+  };
+
+  // Process options and set defaults
+  const options = defaults(args || {}, {
+    outputModifiers: {},
+    tags: {},
+  });
+
+  const defaultTags = {
+    template: function (lang, filePath, node) {
+      return lang || 'html';
+    },
+    script: function (lang, filePath, node) {
+      return 'js';
+    },
+    style: function (lang, filePath, node) {
+      return 'css';
+    }
+  }
+
+  // Add defaultTags that haven't been overriden
+  for (let tag in defaultTags) {
+    if (! options.tags.hasOwnProperty(tag)) {
+      options.tags[tag] = defaultTags[tag];
+    }
+  }
+
+  // Handle file in stream
+  return through.obj(function (file, encode, callback) {
+    // Check that we can handle the stream
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported'));
+      return callback();
+    }
+
+    // Get a reference to the main file stream
+    let mainStream = this;
+
+    // Reset languages and extensions
+    languages, extensions = {};
+
+    // Pass on user configuration to Vue
+    if (options) {
+      compiler.applyConfig(options);
+    }
+
+    // Compile the file
+    compile(file.contents.toString(), file.path, function (err, result) {
+      if (err) {
+        let message = 'In file ' + path.relative(process.cwd(), file.path) + ':\n' + err.message;
+        mainStream.emit('error', new PluginError(PLUGIN_NAME, message));
+        return callback();
+      }
+
+      let fileName = path.basename(file.path, path.extname(file.path));
+      let partialPath = path.join(path.dirname(file.path), fileName);
+      let streams = [];
+
+      // Process tags
+      for (let tag in options.tags) {
+        if (result[tag]) {
+          let content = modify(tag, result[tag], languages[tag]);
+          let extension = extensions[tag] || 'txt';
+          streams.push(createStream(partialPath+'.'+extension, file.base, new Buffer(content)));
+        }
+      }
+
+      // Add component parts to main stream
+      if (streams.length) {
+        // Use the counter to verify when we're done with the last file
+        let counter = streams.length;
+
+        streams.forEach(function(stream) {
+          stream.pipe(through.obj(function(part, enc, cb) {
+            // Add component part to stream
+            mainStream.push(part);
+
+            // Mark as finished
+            --counter || callback();
+          }));
+        });
+      }
+    });
+  });
+
+  function compile(content, filePath, cb) {
+    // path is optional
+    if (typeof filePath === 'function') {
+      cb = filePath;
+      filePath = process.cwd();
+    }
+
+    // generate css scope id
+    var id = hash(filePath || content);
+
+    // parse the file into an HTML tree
+    var fragment = parse5.parseFragment(content, {locationInfo: true });
+
+    // check node numbers
+    if (! compiler.__get__('validateNodeCount')(fragment)) {
+      return cb(new Error('Only one script tag and one template tag allowed per component file.'));
+    }
+
+    // check for scoped style nodes
+    var hasScopedStyle = fragment.childNodes.some(function (node) {
+      return node.nodeName === 'style' && compiler.__get__('isScoped')(node);
+    })
+
+    // Walk through the top level nodes and check for their
+    // types & languages. If there are pre-processing needed,
+    // push it into a jobs list.
+    Promise.all(fragment.childNodes.map(function (node) {
+      let lang = compiler.__get__('checkLang')(node) || null;
+
+      // Save language and file extension
+      for (let tag in options.tags) {
+        if (node.nodeName === tag) {
+          extensions[tag] = options.tags[tag](lang, filePath, node);
+          languages[tag] = lang;
+        }
+      }
+
+      switch (node.nodeName) {
+        case 'template':
+          return compiler.__get__('processTemplate')(node, filePath, id, hasScopedStyle, content);
+        case 'style':
+          return compiler.__get__('processStyle')(node, filePath, id);
+        case 'script':
+          return compiler.__get__('processScript')(node, filePath, content);
+        default:
+          // Process custom tag
+          for (let tag in options.tags) {
+            if (node.nodeName === tag) {
+              return processCustom(tag, node, filePath, content);
+            }
+          }
+      }
+    })
+    .filter(function (p) { return p }))
+    .then(mergeParts, cb)
+    .catch(cb);
+
+    // Combine the compiler output into a standard object
+    function mergeParts(parts) {
+      var result = {};
+
+      for (let tag in options.tags) {
+        result[tag] = compiler.__get__('extract')(parts, tag);
+      }
+
+      cb(null, result);
+    }
+
+    // Process custom handlers
+    function processCustom(tag, node, filePath, content) {
+      let lang = compiler.__get__('checkLang')(node) || null;
+      let custom = compiler.__get__('checkSrc')(node, filePath) || getRawCustomTemplate(node, content);
+
+      return compiler.__get__('compileAsPromise')(tag, deindent(custom), lang, filePath);
+    }
+
+    // For some reason the custom node doesn't have a content attribute and
+    // getRawTemplate from vueify errors out
+    function getRawCustomTemplate(node, source) {
+      var content = node;
+      var l = content.childNodes.length
+      if (!l) return ''
+      var start = content.childNodes[0].__location.startOffset
+      var end = content.childNodes[l - 1].__location.endOffset
+      return source.slice(start, end)
+    }
+  }
+
+  // Print to console if we're debugging
+  function debug(msg) {
+    if (PLUGIN_DEBUG) {
+      console.log(msg);
+    }
+  }
+
+  // Allow output from default compilers to be modified
+  function modify(type, content, lang) {
+    if (options.outputModifiers.hasOwnProperty(type)) {
+      return options.outputModifiers[type](content, lang);
+    }
+    else {
+      return content;
+    }
+  }
+
+  // Add path and base to stream
+  function parseStream(path, base) {
+    return through.obj(function(file, encode, callback){
+      file.base = base;
+      file.path = path;
+      return callback(null, file);
+    });
+  }
+
+  // Create a stream from string
+  function createStream(path, base, contents) {
+    return strStream(contents).pipe(vinylSource()).pipe(parseStream(path, base));
+  }
+}
